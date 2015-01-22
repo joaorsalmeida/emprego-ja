7fd1d5852804d0eb72a04292f4eb45471a948ea6
Alan R. R. Batista <alanrrb@gmail.com>
2015-01-22 20:21:20 -0200

    POR FAVOR IDENTEM

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,26 +1,26 @@
 class CompaniesController < ApplicationController
   def index
-  	@empresas = Company.all
+    @empresas = Company.all
   end
 
   def show
-  	 @empresa = Company.find(params[:id])
+    @empresa = Company.find(params[:id])
   end
 
   def new
     @empresa = Company.new
-	end 
-	
-	def create
-		@empresa = Company.new(empresa_params)
+  end
+
+  def create
+    @empresa = Company.new(empresa_params)
     if @empresa.save 
       redirect_to @empresa
-    else 
+    else
       render "new"
     end
   end
 
-    def empresa_params
+  def empresa_params
     params.require(:company).permit(:fantasy_name, :cnpj, :url, :email, :phone)
-   	end
+  end
 end
