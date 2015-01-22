a7a682182671e02f0eb7a0409ffa8b9075ba4a11
Yomestre <yomestre@hotmail.com>
2015-01-21 22:25:40 -0200

    Validations

--- app/models/job_position.rb
+++ app/models/job_position.rb
@@ -1,2 +1,5 @@
 class JobPosition < ActiveRecord::Base
+	validates_presence_of :title, :description, :location, :expiration_date, :job_status
+	validates_length_of :title, maximum: 100
+	validates :job_status, inclusion: { in: %w(Ativa Desativada Cancelada)}
 end
