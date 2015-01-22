91d69e0fc636503ba24b9af89ee95d88a0956667
Andre <andre.marques212747@hotmail.com>
2015-01-22 19:28:27 -0200

    Criação de validação quantidade de caracteres campo cnpj

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,9 +2,7 @@ class Company < ActiveRecord::Base
 
 	validates_presence_of :fantasy_name, :cnpj, :url, :email, :phone
   	validates_numericality_of :cnpj, :phone
-   	validates_length_of :cnpj, is: 14, :maximum => 15
-   	
-
+   	validates_length_of :cnpj, is: 14
 
 end
 
