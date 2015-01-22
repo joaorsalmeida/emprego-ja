8e31cc3295305117acedafdb108ae31ea2dd119c
Diego Alves <diego.oborba@gmail.com>
2015-01-22 20:36:10 -0200

    validate email format

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,5 @@ class Company < ActiveRecord::Base
 	validates_presence_of :fantasy_name, :cnpj, :url, :email, :phone
   	validates_numericality_of :cnpj, :phone
    	validates_length_of :cnpj, is: 14
-
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 end
-
-	
