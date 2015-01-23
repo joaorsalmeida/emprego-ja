5b8e75bebc9cf65655739d57780ed93a36f65381
Diego Alves <diego.oborba@gmail.com>
2015-01-23 19:42:40 -0200

    unique cnpj value

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,5 @@ class Company < ActiveRecord::Base
   validates_length_of :cnpj, is: 14
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   validates_format_of :url, with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/, on: :create
+  validates_uniqueness_of :cnpj
 end
