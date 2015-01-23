71fb43af148b511694f4df3eba49489d113727c7
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-01-23 21:48:17 -0200

    List job posiitons details

--- app/controllers/job_positions_controller.rb
+++ app/controllers/job_positions_controller.rb
@@ -6,4 +6,13 @@ class JobPositionsController < ApplicationController
   def new
     @job_position = JobPosition.new
   end
+
+  def show
+    @job_position = JobPosition.find(params[:id])
+  end
+
+  def job_position_params
+    params.require(:job_position).permit(:title, :description, :expiration_date, :loction, :job_status)
+  end
+
 end
