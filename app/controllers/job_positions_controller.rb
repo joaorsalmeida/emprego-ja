373b5e6f1c671aa8dfbd0aa966e0e8d32f5d166f
Yomestre <yomestre@hotmail.com>
2015-01-23 20:12:46 -0200

    adcionado view new e um formulario

--- app/controllers/job_positions_controller.rb
+++ app/controllers/job_positions_controller.rb
@@ -5,5 +5,19 @@ class JobPositionsController < ApplicationController
 
   def new
     @job_position = JobPosition.new
+    @min_expiration = Date.today
+    @max_expiration = (Date.today + 90)
+  end
+
+	def create
+		@job_position = JobPosition.new(job_position_params)
+  	if @job_position.save 
+    	redirect_to @job_position
+  	else 
+    	render "new"
+  	end
+	end
+	def job_position_params
+    params.require(:Job_position).permit(:title, :description, :location, :expiration_date, :job_status)
   end
 end
