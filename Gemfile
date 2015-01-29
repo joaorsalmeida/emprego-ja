d4a348403f7bcc65059a865d0d7693c71b56747a
Andre <andre.marques212747@hotmail.com>
2015-01-28 22:09:58 -0200

    Nova Gem CNPJ_Validator, criação validação cnpj

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
 
 gem 'rails-i18n', '~> 4.0.0'
 
+gem "cnpj_validator"
+
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,5 @@ class Company < ActiveRecord::Base
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   validates_format_of :url, with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/, on: :create
   validates_uniqueness_of :cnpj
+  validates :cnpj, :cnpj => true
 end
