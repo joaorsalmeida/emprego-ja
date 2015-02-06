36b7cb687ac7d11c0dca90237fc32b8e3da4d8ab
Yomestre <yomestre@hotmail.com>
2015-02-06 21:15:58 -0200

    Teste de validação logged in

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe CompaniesController, :type => :controller do
   let(:cnpj_valido1) { 67286015000139 }
   let(:cnpj_valido2) { 50304411000149 }
   let(:vaga) {{:title => "Vaga 1",  :description => "descrição um 2 tres", :location => "zona sul", :expiration_date => "01/03/2015", :job_status => "Ativa"}}
+  let(:company1) {{:fantasy_name => "nome teste",   :cnpj => cnpj_valido1, :url => "www.teste", :email => "eumesmo@eu.com", :phone => "23232373"}}
+  let(:company2) {{:fantasy_name => "nome teste",   :cnpj => cnpj_valido2, :url => "www.teste", :email => "eumesmo@eu.com", :phone => "23232373"}}
   describe "General Admin signed in" do
     before do 
       @admin = GeneralAdmin.create!({
-      :email => 'user@test.com',
+      :email => 'users@test.com',
       :password => '12please',
       :password_confirmation => '12please' 
      })
@@ -21,8 +23,8 @@ RSpec.describe CompaniesController, :type => :controller do
       end
 
       it "Should load all companies" do
-      	c1= Company.create!({:fantasy_name => "nome teste",   :cnpj => cnpj_valido1, :url => "www.teste", :email => "eumesmo@eu.com", :phone => "23232373"})
-      	c2= Company.create!({:fantasy_name => "nome teste 2", :cnpj => cnpj_valido2, :url => "www.teste", :email => "emo@eu.com", :phone => "99929999"})
+        c1 = Company.create!(company1)
+        c2 = Company.create!(company2)
         get :index
         expect(assigns(:empresas)).to match_array([c2, c1]) 
       end
@@ -30,7 +32,8 @@ RSpec.describe CompaniesController, :type => :controller do
 
     describe "GET jobs list" do
       it "List jobs" do
-        @c1= Company.create!({:fantasy_name => "nome teste",   :cnpj => cnpj_valido1, :url => "www.teste", :email => "eumesmo@eu.com", :phone => "23232373"})
+        c1 = Company.create!(company1)
+        @c1 = c1
         @j1 = JobPosition.create!(vaga)
         @c1.job_positions << @j1
         @c1.reload
@@ -38,22 +41,31 @@ RSpec.describe CompaniesController, :type => :controller do
         expect(response).to have_http_status(:success)
       end
     end
-    describe "Get New test" do
+    describe "#New" do
       it "Should render new" do
         get :new
         expect(response).to render_template(:new)
       end
     end
-    describe "" do
-      it "" do
-        
+    describe "#Create" do
+      it "Should create a company" do
+        company1created = Company.create!(company1)
+        post :create , :company => company1
+        expect(Company.first).to eq(company1created)
       end 
+    end
+    describe "#Edit" do
+      it "Should render edit" do
+        c1 = Company.create!(company1)
+        get :edit, :id => c1.id
+        expect(response).to render_template(:edit)
+      end
     end
 
 
     describe "Post Update test" do
       it "Company updated" do
-        c1= Company.create!({:fantasy_name => "nome teste", :cnpj => cnpj_valido1, :url => "www.teste", :email => "eumesmo@eu.com", :phone => "23232373"})
+        c1 = Company.create!(company1)
         put :update, :id => c1.id, :company => {:fantasy_name => "vitor novo", :cnpj => cnpj_valido1, :url => "www.teste", :email => "eumesmo@eu.com", :phone => "23232373"}
         c1.reload
         expect(c1.fantasy_name).to eq("vitor novo")
